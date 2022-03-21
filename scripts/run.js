047184bc2af9a2f664c63bb0a4530c2a2f27f2fa
Vigneshwaran Elangovan <vygneshwaran@gmail.com>
2022-03-21 10:49:33 +0530

    update class names

--- scripts/run.js
+++ scripts/run.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const RoyaltySmartContract = await hre.ethers.getContractFactory("RoyaltySmartContract");
+  const royalty = await RoyaltySmartContract.deploy();
 
-  await greeter.deployed();
+  await royalty.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("RoyaltySmartContract deployed to:", royalty.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
