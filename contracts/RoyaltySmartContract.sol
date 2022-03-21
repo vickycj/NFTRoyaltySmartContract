1494e59cc01ecf81564997fff19f1eb09d7f1505
Vigneshwaran Elangovan <vygneshwaran@gmail.com>
2022-03-21 11:41:22 +0530

    update base contract

--- contracts/RoyaltySmartContract.sol
+++ contracts/RoyaltySmartContract.sol
@@ -1,8 +1,14 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
+import "hardhat/console.sol";
 
+contract RoyaltySmartContract is ERC721Royalty, Ownable {
 
-contract RoyaltySmartContract {
+    constructor() ERC721("AviumWorldTestNet", "AW") {
+        console.log("RoyaltySmartContract Constructor");
+    }
     
 }
